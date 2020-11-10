e50da29fb9b79fe03d0eb0dae512336b5be00229
mati360 <72567318+mati360@users.noreply.github.com>
2020-11-10 09:37:02 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//pobranie referencji do przycisku
+let button = document.querySelector('#bg');
+
+//załączenie nasłuchiwacza
+button.addEventListener('click', ()=> {
+  //pobranie referencji do tła strony
+  let body = document.getElementsByTagName('body')[0];
+  
+  //zmiana tła strony - losowanie kolorów
+  body.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+});
